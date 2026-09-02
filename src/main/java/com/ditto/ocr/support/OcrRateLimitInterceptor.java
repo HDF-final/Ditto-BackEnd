2054654e93629c7c1dea3e2d49af8f70479fcb7c
Jeongmin An <ajm010402@gmail.com>
2026-09-02 18:04:40 +0900

    fix: OCR 레이트 리밋 인터셉터가 @WebMvcTest 슬라이스를 깨뜨리지 않게 수정

--- src/main/java/com/ditto/ocr/support/OcrRateLimitInterceptor.java
+++ src/main/java/com/ditto/ocr/support/OcrRateLimitInterceptor.java
@@ -1,6 +1,5 @@
 package com.ditto.ocr.support;
 
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -16,8 +15,12 @@ import lombok.RequiredArgsConstructor;
  *
  * <p>상한을 넘으면 {@link ErrorCode#OCR_RATE_LIMITED}(429)를 던져 컨트롤러 진입 전에 막는다.
  * 예외는 전역 예외 처리기가 표준 에러 응답으로 변환한다.
+ *
+ * <p>스프링 빈으로 자동 감지({@code @Component})하지 않고 {@link OcrWebConfig} 가 직접
+ * 생성해 등록한다. {@code HandlerInterceptor} 빈은 {@code @WebMvcTest} 슬라이스가 모두
+ * 로드하는데, 이 인터셉터를 빈으로 두면 협력 빈 {@link OcrRateLimiter} 가 없는 다른 컨트롤러
+ * 슬라이스 테스트까지 컨텍스트 로딩이 깨지기 때문이다.
  */
-@Component
 @RequiredArgsConstructor
 public class OcrRateLimitInterceptor implements HandlerInterceptor {
 
